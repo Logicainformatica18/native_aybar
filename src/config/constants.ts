316db162e7b348261eff6f8cb9520ca2d580538e
logicainformatica18 <logicainformatica18gmail.com>
2025-06-26 22:02:38 -0500

    update

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,7 +1,7 @@
 // src/config/constants.ts
-export const BASE_IMAGE_URL_USER = 'http://192.168.16.187:8000/api/imageusers/';
-export const BASE_IMAGE_URL = 'http://192.168.16.187:8000/api/uploads/';
-export const BASE_SRC = 'http://192.168.16.187:8000';
+export const BASE_IMAGE_URL_USER = 'http://192.168.1.39:8000/api/imageusers/';
+export const BASE_IMAGE_URL = 'http://192.168.1.39:8000/api/uploads/';
+export const BASE_SRC = 'http://192.168.1.39:8000';
 // constants.ts
-export const API_URL = 'http://192.168.16.187:8000/api'; // sin `/api`, solo el host raíz
+export const API_URL = 'http://192.168.1.39:8000/api'; // sin `/api`, solo el host raíz
 
